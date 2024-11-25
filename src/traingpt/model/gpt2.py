bac004a6c48a34470d4fdae9e7d7fde3291fdc7b
Shane Caldwell <shane@Shanes-MacBook-Pro.local>
2024-11-25 08:49:14 -0600

    Forward pass

--- src/traingpt/model/gpt2.py
+++ src/traingpt/model/gpt2.py
@@ -9,10 +9,22 @@ class GPT2(nn.Module):
         self.token_embedding = nn.Embedding(cfg.vocab_size, cfg.embedding_dim)
         self.position_embedding = nn.Embedding(cfg.context_length, cfg.embedding_dim)
 
-        self.dropout = nn.Dropout(cfg.drop_rate)
+        self.embedding_dropout = nn.Dropout(cfg.drop_rate)
 
         self.transformer_blocks = nn.Sequential(*[TransformerBlock(cfg) for _ in range(cfg.num_layers)])
 
         self.final_norm = LayerNorm(cfg.embedding_dim)
 
         self.out_head = nn.Linear(cfg.embedding_dim, cfg.vocab_size, bias=False)
+
+    def forward(self, token_idx: torch.Tensor) -> torch.Tensor:
+        _, seq_len = token_idx.shape
+        
+        token_embeddings = self.token_embedding(token_idx)
+        position_embeddings = self.position_embedding(torch.arange(seq_len, device=token_idx.device))
+        x = token_embeddings + position_embeddings
+        x = self.embedding_dropout(x) #TODO: maybe make this a seperate config parameter
+        x = self.transformer_blocks(x)
+        x = self.final_norm(x)
+        logits = self.out_head(x)
+        return logits
